a516b3e54c8c9adb20738d660aea2c13f9d1b459
willd <willdwelle@frontier.com>
2022-01-12 17:14:07 -0800

    Form validation

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -6,45 +6,119 @@ const UserForm = (props) => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
-    const [confirmPassword, setConfirmPassword] = useState("");  
+    const [confirmPassword, setConPassword] = useState("");
+    const [FNameError, setFNameError] = useState("");
+    const [LNameError, setLNameError] = useState("");
+    const [EmailError, setEmailError] = useState("");
+    const [PasswordError, setPasswordError] = useState("");
+    const [ConPasswordError, setConPasswordError, setPassCheckError] = useState("");
     
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { firstName, lastName, email, password, confirmPassword };
-        console.log("Welcome", newUser);
-    };
+    
+    const handleFName = (e) => {
+        setFirstName(e.target.value);
+        if(e.target.value.length < 1) {
+            setFNameError("")
+        } else if(e.target.value.length < 2) {
+            setFNameError("First Name must be at least 2 characters!")
+        } else if(e.target.value.length >= 2) {
+            setFNameError("")
+        }
+    }
+
+    const handleLName = (e) => {
+        setLastName(e.target.value);
+        if(e.target.value.length < 1) {
+            setLNameError("")
+        } else if(e.target.value.length < 2) {
+            setLNameError("Last Name must be at least 2 characters!")
+        } else if(e.target.value.length >= 2) {
+            setLNameError("")
+        }
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1) {
+            setEmailError("")
+        } else if(e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters!")
+        } else if(e.target.value.length >= 5) {
+            setEmailError("")
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if(e.target.value.length < 1) {
+            setPasswordError("")
+        } else if(e.target.value.length < 8) {
+            setPasswordError("Password must be at least 8 characters!")
+        } else if(e.target.value.length >= 8) {
+            setPasswordError("")
+        }
+    }
+
+    const handleConPassword = (e) => {
+        setConPassword(e.target.value);
+        if(e.target.value.length < 1) {
+            setConPasswordError("")
+        } else if(e.target.value.length < 8) {
+            setConPasswordError("Password must be at least 8 characters!")
+        } else if(e.target.value.length >= 8) {
+            setConPasswordError("")
+        }
+        if(confirmPassword!== password) {
+            setPassCheckError("Passwords do not match!")
+        } else {
+            setPassCheckError("")
+        }
+    }
     
     return(
         <div>
-            <form onSubmit={ createUser }>
+            <form onSubmit={ (e) => e.preventDefault() }>
                 <div>
                     <label>First Name: </label> 
-                    <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                    <input type="text" onChange={ handleFName } />
+                    {FNameError ?
+                    <p>{FNameError}</p> :
+                    ''
+                    }
                 </div>
                 <div>
                     <label>Last Name: </label> 
-                    <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                    <input type="text" onChange={ handleLName } />
+                    {LNameError ?
+                    <p>{LNameError}</p> :
+                    ''
+                    }
                 </div>
                 <div>
                     <label>Email Address: </label> 
-                    <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                    <input type="text" onChange={ handleEmail } />
+                    {EmailError ?
+                    <p>{EmailError}</p> :
+                    ''
+                    }
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                    <input type="text" onChange={ handlePassword } />
+                    {PasswordError ?
+                    <p>{PasswordError}</p>:
+                    ''
+                    }
                 </div>
                 <div>
-                    <label>Confirm Password: </label>
-                    <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } />
+                    <label> Confirm Password: </label>
+                    <input type="text" onChange={ handleConPassword } />
+                    {ConPasswordError ?
+                    <p>{ConPasswordError}</p>:
+                    ''
+                    }
                 </div>
                 <input type="submit" value="Create User" />
             </form>
-            <h1>USER</h1>
-            <p>First Name: {firstName}</p>
-            <p>Last Name: {lastName}</p>
-            <p>Email: {email}</p>
-            <p>Password: {password}</p>
-            <p>Confirm Password: {confirmPassword}</p>
         </div>
     );
 };
